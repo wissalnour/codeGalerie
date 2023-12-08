4cbe6d3f89b7bd9bc6e664176889f04d0c642b04
wissalnour <152960535+wissalnour@users.noreply.github.com>
2023-12-09 00:58:03 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,73 +1,71 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, request  
+import os  
 from azure.storage.blob import BlobServiceClient
-import os
-from datetime import datetime
-import logging
+from flask import Flask, request, redirect
+app = Flask(__name__)  
+connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING') # retrieve the connection string from the environment variable
+container_name = "photos" # container name in which images will be store in the storage account
+blob_service_client = BlobServiceClient.from_connection_string(conn_str=connect_str)
+try:
+    container_client = blob_service_client.get_container_client(container=container_name) # get container client to interact with the container in which images will be stored
+    container_client.get_container_properties() # get properties of the container to force exception to be thrown if container does not exist
+except Exception as e:
+    container_client = blob_service_client.create_container(container_name)
+     
 
+     
+@app.route("/")
+def view_photos():
+    blob_items = container_client.list_blobs() # list all the blobs in the container
 
-app = Flask(_name_)
-app.config['UPLOAD_FOLDER'] = 'uploads'
+    img_html = "<div style='display: flex; justify-content: space-between; flex-wrap: wrap;'>"
 
-# Replace 'YOUR_AZURE_STORAGE_CONNECTION_STRING' with your actual Azure Storage connection string
-app.config['AZURE_STORAGE_CONNECTION_STRING'] = 'DefaultEndpointsProtocol=https;AccountName=stockgalerie;AccountKey=j+aL4fpCCjaug1yngJcqXibkLQyU+oNe/NbV2j/PQH/ENHwghJCPSo9RIRgfBmdjNAtxHuGYpEQx+ASt7dod+g==;EndpointSuffix=core.windows.net'
-
-# Replace 'uploads' with the name of the container you created in Azure Storage
-app.config['CONTAINER_NAME'] = 'conteneur'
-
-def upload_to_blob(file):
-    blob_service_client = BlobServiceClient.from_connection_string(app.config['AZURE_STORAGE_CONNECTION_STRING'])
-    container_client = blob_service_client.get_container_client(app.config['CONTAINER_NAME'])
-
-    with open(file, 'rb') as data:
-        container_client.upload_blob(name=os.path.basename(file), data=data)
-
-@app.route('/')
-@app.route('/')
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        # Add your authentication logic here
-        # Example: Check if the username and password are correct
-        if username == 'your_username' and password == 'your_password':
-            # Successful login, you might want to redirect to the dashboard
-            logging.info('Successful login for user: %s', username)
-            return redirect(url_for('dashboard'))
-        else:
-            # Failed login, you might want to display an error message
-            error_message = 'Invalid credentials. Please try again.'
-            logging.warning('Failed login attempt for user: %s', username)
-            return render_template('login.html', error_message=error_message)
-    return render_template('login.html')
-
-
-@app.route('/dashboard')
-def dashboard():
-    blob_service_client = BlobServiceClient.from_connection_string(app.config['AZURE_STORAGE_CONNECTION_STRING'])
-    container_client = blob_service_client.get_container_client(app.config['CONTAINER_NAME'])
-    blobs = container_client.list_blobs()
-
-    images = [{'filename': blob.name, 'upload_date': blob.creation_time} for blob in blobs]
-    return render_template('dashboard.html', images=images)
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    if 'file' not in request.files:
-        return redirect(url_for('dashboard'))
-    file = request.files['file']
-    if file.filename == '':
-        return redirect(url_for('dashboard'))
+    for blob in blob_items:
+        blob_client = container_client.get_blob_client(blob=blob.name) # get blob client to interact with the blob and get blob url
+        img_html += "<img src='{}' width='auto' height='200' style='margin: 0.5em 0;'/>".format(blob_client.url) # get the blob url and append it to the html
    
-    if file:
-        # Local storage (optional, depending on your needs)
-        local_filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file.save(local_filename)
+    img_html += "</div>"
 
-        # Upload to Azure Blob Storage
-        upload_to_blob(local_filename)
+    # return the html with the images
+    return """
+    <head>
+    <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container">
+                <a class="navbar-brand" href="/">Photos App</a>
+            </div>
+        </nav>
+        <div class="container">
+            <div class="card" style="margin: 1em 0; padding: 1em 0 0 0; align-items: center;">
+                <h3>Upload new File</h3>
+                <div class="form-group">
+                    <form method="post" action="/upload-photos"
+                        enctype="multipart/form-data">
+                        <div style="display: flex;">
+                            <input type="file" accept=".png, .jpeg, .jpg, .gif" name="photos" multiple class="form-control" style="margin-right: 1em;">
+                            <input type="submit" class="btn btn-primary">
+                        </div>
+                    </form>
+                </div>
+            </div>
+       
+    """ + img_html + "</div></body>"
+ 
+#flask endpoint to upload a photo
+#flask endpoint to upload a photo
+@app.route("/upload-photos", methods=["POST"])
+def upload_photos():
+    filenames = ""
 
-        return redirect(url_for('dashboard'))
-
-if _name_ == '_main_':
-    app.run(debug=True)
+    for file in request.files.getlist("photos"):
+        try:
+            container_client.upload_blob(file.filename, file) # upload the file to the container using the filename as the blob name
+            filenames += file.filename + "<br /> "
+        except Exception as e:
+            print(e)
+            print("Ignoring duplicate filenames") # ignore duplicate filenames
+       
+    return redirect('/') 
